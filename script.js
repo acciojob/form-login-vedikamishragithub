8e02b81199a5987f91bc1580e5fce987dda5c4c8
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-20 01:41:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
-    //Write your code here
-
+ //    let children = document.querySelectorAll('#form1');
+	// let fname=children[0].value;
+	// let lname=children[1].value;
+function getFormvalue(event) {
+    event.preventDefault();
+    let form = document.querySelector('#form1');
+    let fname = form.querySelector('input[name=fname]').value;
+    let lname = form.querySelector('input[name=lname]').value;
+    let fullName = fname + ' ' + lname;
+    alert(fullName);
 }
